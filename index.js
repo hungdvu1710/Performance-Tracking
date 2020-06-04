919ac1f9e543e6a47516b1afa4bab4fa27a668a1
Hung Vu <hungvsme@gmail.com>
2020-06-04 22:32:08 +0700

    style fixes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateMemoryUsage(){
   const memoryUsed = os.totalmem() - os.freemem()
   const displayInGB = Math.floor(memoryUsed/10000000) / 100
   document.querySelector("#memUsed").innerText = `Memory Used: ${displayInGB}GB`
+
   const percentMemoryUsed = ((memoryUsed)/os.totalmem())*100
   document.querySelector("#memory").setAttribute("value",`${percentMemoryUsed}`)
 }
@@ -12,6 +13,7 @@ function updateMemoryUsage(){
 function updateMemoryUsageWithPromise(){
   return new Promise(function(e) {
     updateMemoryUsage()
+    
     if(e) throw e
   })
 }
@@ -20,16 +22,18 @@ function updateCpuUsageWithPromise(){
   return new Promise(function(e) {
     exec('wmic cpu get loadpercentage', (error, stdout) => {
       if (error) {
-        console.error(`exec error: ${error}`);
-        return;
+        console.error(`exec error: ${error}`)
+        return
       }
-      res = stdout.split(" ");
+      const res = stdout.split(" ")
       document.querySelector("#percentCpuUsed").innerText = `CPU used: ${Number(res[2])}%`
       document.querySelector("#cpu").setAttribute("value", Number(res[2]))
     });
+
     if(e) throw e
   })
 }
+
 setInterval(() =>{
   Promise.all([
     updateCpuUsageWithPromise(),
