abd59eed82878c8057285e8bd5514b36d80273cb
hungdvu1710 <hungvsme@gmail.com>
2020-06-05 15:42:51 +0700

    fix disk tracker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const os = require('os')
 const { exec } = require('child_process')
 const GB_TO_BYTE = 1073741824
-
+let numDisk = 0;
 function updateMemoryUsage() {
   const memoryUsed = os.totalmem() - os.freemem()
   const memoryUsedGb = Math.floor(memoryUsed/GB_TO_BYTE * 100) / 100
@@ -29,12 +29,25 @@ function updateDiskUsage(){
       }
 
       const res = stdout.replace(/\s+/g, " ").split(" ")
-      const numDisk = (res.length - 4)/3
-
-      for(let i = 1; i <= numDisk; i++){
-        const diskName = res[i*3]
-        const freeSpace = res[i*3 +1]
-        const totalCapacity = res[i*3+2]
+      let diskIterator = 0
+      for(let i = 3; i < res.length; i++){
+        if(!isNaN(Number(res[i]))) continue //if res[i] is a number, it doesn't represent a disk
+        if(isNaN(Number(res[i+1]))){
+          //if res[i] is a disk, but it has no statistics followed
+          if(document.querySelector(".logicalDiskTracker").children.length < numDisk){
+            document.querySelector(".logicalDiskTracker").innerHTML +=`
+            <div class="disk${res[i]}">
+              <div>Disk: ${res[i]}</div>
+            </div>
+            `
+          } else{
+            diskIterator++
+          }
+          continue
+        }
+        const diskName = res[i]
+        const freeSpace = res[i+1]
+        const totalCapacity = res[i+2]
         const usedSpace = totalCapacity - freeSpace
         const usedSpaceInGb = Math.floor(usedSpace/GB_TO_BYTE * 100) / 100
         const totalCapacityInGb = Math.floor(totalCapacity/GB_TO_BYTE * 100) / 100
@@ -46,21 +59,37 @@ function updateDiskUsage(){
             <div>Disk Capacity: ${totalCapacityInGb}GB</div>
             <div>Used space: ${usedSpaceInGb}GB</div>
             <progress value="${(usedSpace/totalCapacity) * 100}" max="100"></progress>
+            <br>
           </div>
           `
         } else{
-          document.querySelector(".logicalDiskTracker").children[i-1].innerHTML =`
+          document.querySelector(".logicalDiskTracker").children[diskIterator].innerHTML =`
           <div>Disk: ${diskName}</div>
           <div>Disk Capacity: ${totalCapacityInGb}GB</div>
           <div>Used space: ${usedSpaceInGb}GB</div>
           <progress value="${(usedSpace/totalCapacity) * 100}" max="100"></progress>
+          <br>
           `
+          diskIterator++
         }
       }
 
       resolve()
     });
   })
+}
+
+function getNumDisk(){
+  exec('wmic logicaldisk get caption', (error, stdout) => {
+    if (error) {
+      reject(error)
+      console.error(`exec error: ${error}`)
+      return
+    }
+  
+    const res = stdout.replace(/\s+/g, " ").split(" ")
+    numDisk = res.length - 2
+  });
 }
 
 function updateCpuUsage(){
@@ -84,13 +113,14 @@ const updateUi = () =>{
   Promise.all([
     updateCpuUsage(),
     updateMemoryUsage(),
-    updateDiskUsage(),
+    updateDiskUsage()
   ])
   .catch(console.error)
 
   document.querySelector("#uptime").innerText = os.uptime()
 }
 
+getNumDisk()
 getCpuInfo()
 updateUi()
 setInterval(updateUi, 1000)
